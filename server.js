d4e722efacff98c90c091588bed9bafdff994c94
DanielleMatthews <danielleem120@gmail.com>
2022-03-08 16:00:29 -0500

    commiyt

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use((req, res, next)=>{
 })
 
 //keep this near the top 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extenDed:true}))
 
 //set up view engine above routes
 app.set('view engine', 'jsx')
