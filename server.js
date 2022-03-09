0d1221cc3f83187fb82566dcec7f9ae7bb6c92e5
DanielleMatthews <danielleem120@gmail.com>
2022-03-09 09:51:35 -0500

    commit

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/fruits/', (req, res)=>{
 })
 
 //connect to mongo database
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo')
 })
